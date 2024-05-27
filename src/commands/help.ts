7ce1c22168851368caeb390338ad7ab44ff5afc6
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-05-27 17:08:47 +0630

    Mute command added in help.

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -13,6 +13,7 @@ module.exports = {
                 { name: '```!warn @user```', value: 'Warn a user. After 3 warnings, the user will be muted.' },
                 { name: '```!repeat```', value: 'Toggle repeat mode. When enabled, the bot will repeat every message sent by members.' },
                 { name: '```!repeat --test```', value: 'Toogle repeat mode for testing purpose. When enabled, the bot will repeat every message sent in the test channel.' },
+                { name: '```!mute @user```', value: 'Mute a user.' },
             );
 
         message.channel.send({ embeds: [helpEmbed] });
